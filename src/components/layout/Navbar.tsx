037301808a7e8220f2c9cb1a0b682ae7287ff96c
Cesar Arteaga <c_arteaga94@hotmail.com>
2025-12-08 23:28:44 -0500

    fix navbar animation + subtitle

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,41 +1,52 @@
 "use client";
 import { gsap, useGSAP } from "@/src/lib/gsap";
 
-const resizableBoxClassname = "resizable-box w-[40px] h-[110px]";
+const resizableBoxClassname = "resizable-box w-[40px] h-[110px] ";
 
 const menuItems = ["Inicio", "Contenido", "Equipo", "Contacto"];
 
 export default function Navbar() {
   useGSAP(() => {
     const tl = gsap.timeline();
-    tl.to(".resizable-box", {
-      height: 0,
-      delay: 1.4,
-      duration: 0.6,
-      ease: 'circ.out',
-      stagger: {
-        each: 0.6, // <-- one finishes before next starts
+
+    tl.fromTo(
+      ".ani",
+      {
+        y: 50,
       },
-    });
+      {
+        y: 0,
+        opacity: 1,
+        delay: 1.1,
+        ease: "circ.out",
+        stagger: {
+          each: 0.3, // <-- one finishes before next starts
+        },
+      }
+    );
   }, []);
 
   return (
-    <div className="flex items-center justify-between h-[60px] absolute top-0 left-0 right-0 overflow-clip">
-      <div>
-        <div className={resizableBoxClassname}></div>
-        <img
-          src={"/assets/logos/pulso-verde-blanco-x1.png"}
-          className="h-[50px]"
-        />
+    <div className="flex top-0 left-0 right-0 absolute justify-center">
+      <div className="min-w-[800px] w-[1100px] max-w-[1200px] flex items-center justify-between h-[60px] overflow-hidden">
+        <div className="flex">
+          <img
+            src={"/assets/logos/pulso-verde-blanco-x1.png"}
+            className="ani h-[50px] opacity-0"
+          />
+        </div>
+
+        <div className="text-white flex w-[300px] justify-between">
+          {menuItems.map((menu) => (
+            <div key={menu} className="flex">
+              <span className="ani opacity-0">{menu}</span>
+            </div>
+          ))}
+        </div>
       </div>
 
-      <div className="text-white flex justify-evenly w-[300px]">
-        {menuItems.map((menu) => (
-          <div key={menu}>
-            <div className={resizableBoxClassname}></div>
-            <span>{menu}</span>
-          </div>
-        ))}
+      <div>
+        <span></span>
       </div>
     </div>
   );
--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -1,36 +1,56 @@
+"use client";
 import { gsap, useGSAP } from "@/src/lib/gsap";
 
 export default function Hero() {
-  const fontSize = "text-[170px]";
-  const boxSize = "h-[155px]";
-
   useGSAP(() => {
-    gsap.to(".resizesable-box", {
-      height: "0px",
-      ease: "circ.out",
-      delay: "0.6",
-    });
+    const tl = gsap.timeline();
+
+    tl.fromTo(
+      ".animated-text",
+      {
+        y: 200,
+      },
+      { y: 0, ease: "circ.out", opacity: 1 }
+    );
+
+    gsap.fromTo(
+      ".subtitle",
+      {
+        y: 200,
+      },
+      { y: 0, opacity: 1, delay: 0.5, ease: "circ.out" }
+    );
   }, []);
 
   return (
     <div className="hero-bg">
-      <div className="hero-backdrop">
-        <div className="hero-content text-white p-12">
-          <div className={`${boxSize} overflow-clip`}>
-            <div className={`resizesable-box ${boxSize}`}></div>
-            <span
-              className={`${fontSize} leading-none tracking-[0px]`}
-            >
-              Pulso
-            </span>
-          </div>
-          <div className={`${boxSize} overflow-clip`}>
-            <div className={`resizesable-box ${boxSize}`}></div>
-            <span
-              className={`${fontSize} leading-none tracking-[0px]`}
-            >
-              Verde
-            </span>
+      <div className="hero-backdrop flex justify-center">
+        <div className="flex-1 max-w-[1100px]">
+          <div className="h-[120px]" />
+          <div className="text-white flex flex-1 flex-col h-[85vh]">
+            <div className="flex flex-col flex-3">
+              <div className="flex h-[110px] overflow-hidden">
+                <span
+                  className={`animated-text text-[120px] leading-none tracking-[0px] opacity-0`}
+                >
+                  Pulso
+                </span>
+              </div>
+              <div className="flex h-[110px] overflow-hidden">
+                <span
+                  className={`animated-text text-[120px] leading-none tracking-[0px] opacity-0`}
+                >
+                  Verde
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-1">
+              <div className="h-[100px] overflow-hidden">
+                <div className="subtitle text-white uppercase max-w-[200px] opacity-0">
+                  Sostenibilidad cool Sostenibilidad cool Sostenibilidad cool
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
